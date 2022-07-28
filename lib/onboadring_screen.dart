52a598a8d1fbab40b12ec7d1edfe0f60b08c4de7
STANKILLAH <106737348+STANKILLAH@users.noreply.github.com>
2022-07-29 00:03:28 +0800

    update onboarding_screen.dart

--- lib/onboadring_screen.dart
+++ lib/onboadring_screen.dart
@@ -17,14 +17,18 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
             color: const Color.fromARGB(255, 251, 255, 0),
             child: Column(
               children: [
-                Padding(
-                  padding: const EdgeInsets.all(30),
-                  child: Image.asset('assets/image/CZ.png'),
-                ),
                 const Center(
                   child: Text(
                     'Welcome To ChatZ',
-                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(30),
+                  child: Image.asset(
+                    'images/CZ.png',
+                    height: 300,
+                    width: 300,
                   ),
                 ),
                 const SizedBox(
@@ -38,13 +42,116 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
             ),
           ),
           Container(
-            color: Color.fromARGB(255, 255, 0, 140),
+            color: const Color.fromARGB(255, 255, 0, 140),
+            child: Column(
+              children: [
+                const Center(
+                  child: Text(
+                    'Messaging',
+                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(30),
+                  child: Image.asset(
+                    'images/Second screen.png',
+                    height: 300,
+                    width: 300,
+                  ),
+                ),
+                const SizedBox(
+                  child: Text(
+                    'Sheeeeesh this page is cracked',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+                  ),
+                ),
+              ],
+            ),
           ),
           Container(
             color: const Color.fromARGB(255, 0, 255, 8),
+            child: Column(
+              children: [
+                const Center(
+                  child: Text(
+                    'Video and Voice calls',
+                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(30),
+                  child: Image.asset(
+                    'images/3rd Screen.png',
+                    height: 300,
+                    width: 300,
+                  ),
+                ),
+                const SizedBox(
+                  child: Text(
+                    'There os 2 more pages after this page',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+                  ),
+                ),
+              ],
+            ),
           ),
           Container(
             color: const Color.fromARGB(255, 255, 81, 0),
+            child: Column(
+              children: [
+                const Center(
+                  child: Text(
+                    'Live Streaming',
+                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(30),
+                  child: Image.asset(
+                    'images/livescreen.jpg',
+                    height: 300,
+                    width: 300,
+                  ),
+                ),
+                const SizedBox(
+                  child: Text(
+                    'Go live to meet new people around the world',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            color: const Color.fromARGB(255, 255, 81, 0),
+            child: Column(
+              children: [
+                const Center(
+                  child: Text(
+                    'Stories',
+                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(30),
+                  child: Image.asset(
+                    'images/Stories.jpg',
+                    height: 300,
+                    width: 300,
+                  ),
+                ),
+                const SizedBox(
+                  child: Text(
+                    'This is the last page of the onboarding screen',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
